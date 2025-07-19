dc97e0ab19ecc8fd20269d7173bc4596ccd7e734
Akash Wadode <70871322+akashwadode@users.noreply.github.com>
2025-07-19 20:42:36 +0530

    Create Bi Contest 161 - Split Array by Prime Indices.cpp

---
+++ Bi Contest 161 - Split Array by Prime Indices.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long splitArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool>isPrime(n,true);
+        if (n > 0)
+            isPrime[0] = false;
+        if (n > 1)
+            isPrime[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        long long sumA = 0, sumB = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (isPrime[i]) {
+                sumA += nums[i];
+            } else {
+                sumB += nums[i];
+            }
+        }
+        return abs(sumA - sumB);
+    }
+};
